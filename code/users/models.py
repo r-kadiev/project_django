6dea8fc92d6b116d3e863a200003e69ffc284507
rizvan <rizvan@MacBook-Pro-rizvan.local>
2023-05-14 22:32:33 +0300

    fix_models

--- code/users/models.py
+++ code/users/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     class Meta:
-        verbose_name = "Пользоватеddль"
+        verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
