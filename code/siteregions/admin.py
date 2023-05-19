0e2869da483167eb71260312710fb1251f3bcafb
rizvan <rizvan@MacBook-Pro-rizvan.local>
2023-05-19 16:24:51 +0300

    corrected_admins.py

--- code/siteregions/admin.py
+++ code/siteregions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Site, Region, SiteRegion
 
-# Register your models here.
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['domain']
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class SiteRegionAdmin(admin.ModelAdmin):
+    list_display = ['region_id', 'site_id', 'type', 'key', 'value']
+
+
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(SiteRegion, SiteRegionAdmin)
