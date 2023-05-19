48b437a96e287abc82ca720618ca26b3160dd348
rizvan <rizvan@MacBook-Pro-rizvan.local>
2023-05-19 16:24:08 +0300

    corrected_urls.py

--- code/project/urls.py
+++ code/project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('site-region/', include('siteregions.urls')),
 ]
